439cf5e6f2251ef99841fa917e9c6954e2ed2665
TeemoCaption <eyan0816@gmail.com>
2023-12-21 18:39:01 +0800

    update

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <h1>{{ title }}</h1>
+  <p>Welcome....</p>
   <input type="text" ref="name">   <!--使用 ref 屬性給元素或子組件添加一個參考名稱-->
   <button @click="handleClick">Click me</button>
-  <Modal :header="header" :text="hello" theme="sale"></Modal>
+  <div v-if="showModal">
+    <!-- v-bind的縮寫是「:」，用於動態地綁定一個或多個屬性，或者一組屬性到表達式-->
+    <!-- 在使用子組件(Modal.vue)的地方用 @close 或 v-on:close 來監聽這個「close」事件 -->
+    <Modal :header="header" :text="hello" theme="sale" @close="togleModal"></Modal>  
+  </div>
+  <button @click.alt="togleModal">open Modal(alt)</button>
 </template>
 
 <script>
 import Modal from './components/Modal.vue';
 
 
-export default {
+export default {  // export 導出物件
   name: 'App',
-  components:{
+  components:{  // components 屬性用於在 Vue 實例或組件中註冊子組件
     Modal
   },
   data() {
     return {
       title: 'My first vue app :)',
       header: 'Sign up for the giveaway!',
-      text: 'hello'
+      text: 'hello',
+      showModal: false,
     }
   },
   methods: {
@@ -26,6 +33,9 @@ export default {
       console.log(this.$refs.name);   // 通過 this.$refs 來訪問元素或子組件
       this.$refs.name.classList.add('active');  // 使用classList.add('...') 來添加一個class
       this.$refs.name.focus();  // focus() 聚焦在輸入框
+    },
+    togleModal() {
+      this.showModal = !this.showModal;
     },
   }
 }
--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="backdrop">
-        <div class="modal" :class="{ sale: theme === 'sale'}">
+     <!--@click.self 只有當事件是直接在該元素上觸發（而不是其子元素上）時，事件才會被執行。-->
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale'}">  <!--條件表達式-->
             <h1>{{ header }}</h1>
             <p>{{ text }}</p>
         </div>
@@ -8,8 +9,14 @@
 </template>
 
 <script>
-export default{
-    props: ['header','text','theme']
+export default{  // export 導出物件
+    // prop 是一種允許從父組件向子組件傳遞資料的機制，當父組件使用這個子組件時，它可以通過屬性的方式向這三個 prop 傳遞數據。
+    props: ['header','text','theme'],  
+    methods: {
+        closeModal(){  // 關閉模態框
+            this.$emit('close');  // $emit 用於觸發（emit）一個事件。這個方法主要用於子組件，以便通知其父組件某件事情已經發生。
+        },
+    },
 }
 
 </script>
