d9bd3ea7a0708be6aa8a9c87d587543ff024f3d2
TeemoCaption <eyan0816@gmail.com>
2023-12-25 02:15:55 +0800

    update

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  
+</template>
+    <div class="block">
+        click me
+    </div>
+<script>
+export default {
+    prop: ['delay'],
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
