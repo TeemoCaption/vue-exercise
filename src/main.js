684abc2ccb6a03218c691b2c55fb9983996ee5a7
TeemoCaption <eyan0816@gmail.com>
2024-01-31 22:02:56 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // 預設導入router內的index.js
 
 createApp(App).use(router).mount('#app')
