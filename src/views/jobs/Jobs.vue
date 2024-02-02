0c47bf19ea7fd351e5f8c9de5f9e58ce6e572fa2
TeemoCaption <eyan0816@gmail.com>
2024-02-02 17:26:18 +0800

    update

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -14,7 +14,16 @@ export default {
         return {
             jobs: [],
         }
-    }
+    },
+    // 當組件完成了所有的初始化工作，並把處理好的 HTML 插入到頁面上相應的位置後，mounted() 生命週期鉤子就會被調用，mounted() 常常用來執行那些需要訪問或操作 DOM 的任務，只有在這個階段，組件的 DOM 結構才已經形成
+    mounted(){
+        // 使用 fetch API 向本地伺服器的 '/jobs' 端點發送 GET 請求
+        
+        fetch('http://localhost:3000/jobs')
+            .then((res)=> res.json())  // 得到響應後，將響應體（Response Body）解析為 JSON 格式
+            .then(data => this.jobs = data)  // 將解析後的 JSON 數據（即 'jobs' 數據）賦值給組件的 'jobs' 屬性
+            .catch(err => console.log(err.message))
+        }       
 }
 </script>
 
